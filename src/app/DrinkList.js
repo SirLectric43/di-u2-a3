1b33953a8ed4e04cbd79169a354ae1ec8584404d
SirLectric43 <domingomiguel4343@gmail.com>
2025-10-29 09:17:17 +0100

    cambioEj3

--- src/app/DrinkList.js
+++ src/app/DrinkList.js
@@ -1,25 +1,36 @@
 function Drink({ name }) {
-    return (
-      <section>
-        <h1>{name}</h1>
-        <dl>
-          <dt>Parte de la planta</dt>
-          <dd>{name === 'té' ? 'hoja' : 'grano'}</dd>
-          <dt>Contenido de cafeína</dt>
-          <dd>{name === 'té' ? '15–70 mg/taza' : '80–185 mg/taza'}</dd>
-          <dt>Antigüedad</dt>
-          <dd>{name === 'té' ? '4,000+ años' : '1,000+ años'}</dd>
-        </dl>
-      </section>
-    );
+  let parte, cafeina, antiguedad;
+
+  if (name === 'té') {
+    parte = 'hoja';
+    cafeina = '15–70 mg/taza';
+    antiguedad = '4,000+ años';
+  } else {
+    parte = 'grano';
+    cafeina = '80–185 mg/taza';
+    antiguedad = '1,000+ años';
   }
-  
-  export default function DrinkList() {
-    return (
-      <div>
-        <Drink name="té" />
-        <Drink name="café" />
-      </div>
-    );
-  }
-  
+
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Parte de la planta</dt>
+        <dd>{parte}</dd>
+        <dt>Contenido de cafeína</dt>
+        <dd>{cafeina}</dd>
+        <dt>Antigüedad</dt>
+        <dd>{antiguedad}</dd>
+      </dl>
+    </section>
+  );
+}
+
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="té" />
+      <Drink name="café" />
+    </div>
+  );
+}
