dc017f674e7a9b4fadfe4d07452352e040c44f38
SirLectric43 <domingomiguel4343@gmail.com>
2025-10-29 09:16:24 +0100

    cambioEj2

--- src/app/PackaginList2.js
+++ src/app/PackaginList2.js
@@ -1,30 +1,30 @@
 function Item({ name, importance }) {
-    return (
-      <li className="item">
-        {name}
-      </li>
-    );
-  }
-  
-  export default function PackingList2() {
-    return (
-      <section>
-        <h1>Lista de equipaje de Sally Ride</h1>
-        <ul>
-          <Item 
-            importance={9} 
-            name="Traje de vuelo" 
-          />
-          <Item 
-            importance={0} 
-            name="Casco con dorado a la hoja" 
-          />
-          <Item 
-            importance={6} 
-            name="Fotografía de Tam" 
-          />
-        </ul>
-      </section>
-    );
-  }
-  
+  return (
+    <li className="item">
+      {name}
+      {importance !== 0 && <i> (Importancia: {importance})</i>}
+    </li>
+  );
+}
+
+export default function PackingList2() {
+  return (
+    <section>
+      <h1>Lista de equipaje de Sally Ride</h1>
+      <ul>
+        <Item 
+          importance={9} 
+          name="Traje de vuelo" 
+        />
+        <Item 
+          importance={0} 
+          name="Casco con dorado a la hoja" 
+        />
+        <Item 
+          importance={6} 
+          name="Fotografía de Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
